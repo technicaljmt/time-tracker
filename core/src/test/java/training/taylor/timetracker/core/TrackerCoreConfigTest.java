5bab4eb40ff9932e7e08099a944f538345594678
technicaljmt <priyabhadauriya700@gmail.com>
2025-02-20 18:36:53 +0530

    Update TrackerCoreConfigTest.java

--- core/src/test/java/training/taylor/timetracker/core/TrackerCoreConfigTest.java
+++ core/src/test/java/training/taylor/timetracker/core/TrackerCoreConfigTest.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import training.taylor.timetracker.core.dao.TimeEntry;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 
 import java.util.List;
 
